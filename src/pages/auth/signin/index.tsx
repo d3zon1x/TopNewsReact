c5f69597c409a67ebe848283918c9da9fec1daee
Sost_sg54@student.itstep.org <Sost_sg54@student.itstep.org>
2023-09-25 20:44:45 +0300

    add service and valid

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -13,8 +13,10 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useFormik, Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
+import { LoginSchema } from '../validation';
+import { login } from '../../../services/api-user-service';
 
+const initialValues = { email: "", password: "", rememberMe: false };
 
 function Copyright(props: any) {
   return (
@@ -29,50 +31,23 @@ function Copyright(props: any) {
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
+
 const defaultTheme = createTheme();
-// const validate = (values: { email: string; }) => {
-//   const errors = {};
-
-//   if (!values.email) {
-//     errors.email = 'Required';
-//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-//     errors.email = 'Invalid email address';
-//   }
-
-//   return errors;
-// };
-const SignupSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required('Required'),
-  password: Yup.string().required('Required').min(6, 'password cant be < 6').max(20, 'password is too long')
-});
-
-// const formik = useFormik({
-//   initialValues: {
-//     email: '',
-//   },
-//   onSubmit: values => {
-//     alert(JSON.stringify(values, null, 2));
-//   },
-// });
 
 export default function SignIn() {
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: ''
-    },
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
+    const user = {
+        email: data.get("email"),
+        password: data.get("password"),
+        rememberMe: true,
+    };
+    login(user);
   };
   
  
@@ -83,85 +58,79 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
           <Formik
-              initialValues={{
-                email: '',
-                password: ''
-              }}
-              validationSchema={SignupSchema}
-              onSubmit={values => {
-                // same shape as initial values
-                console.log(values);
-              }}
-            >
-          {({ errors, touched }) => (
-          <Box component="form" onSubmit={formik.handleSubmit} noValidate sx={{ mt: 1 }}>
-              {/* <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={formik.handleChange}
-              value={formik.values.email}
-            /> */}
-            <TextField name="email" type="email" margin="normal"
-              required
-              fullWidth />
-           {errors.email && touched.email ? <div>{errors.email}</div> : null}
-           
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link to="/" >
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link to="/signup">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-          )}
-        </Formik>
+            initialValues={initialValues}
+            onSubmit={() => {}}
+            validationSchema={LoginSchema}
+          >
+            {({ errors, touched, isSubmitting, isValid, dirty }) => (
+              <Box
+                onSubmit={handleSubmit}
+                component="form"
+                noValidate
+                sx={{ mt: 1 }}
+              >
+                  <Field
+                    as={TextField}
+                    margin="normal"
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                  />
+                  {errors.email && touched.email ? (
+                    <div style={{ color: "red" }}>{errors.email}</div>
+                  ) : null}
+                <Field
+                  as={TextField}
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                {errors.password && touched.password ? (
+                  <div style={{ color: "red" }}>{errors.password}</div>
+                ) : null}
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  disabled={!(isValid && dirty)}
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link to="/">Forgot password?</Link>
+                  </Grid>
+                  <Grid item>
+                    <Link to="/signup">{"Don't have an account? Sign Up"}</Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            )}
+          </Formik>
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -12,121 +12,161 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Formik, Field } from "formik";
+import { RegisterSchema } from '../validation';
+import { login } from '../../../services/api-user-service';
+
+
+const initialValues = { firstName: "", lastName: "", email: "", password: "", confirmPassword: "" };
 
 function Copyright(props: any) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" to="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" to="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
-export default function SignUp() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link to="/signin">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
-  );
+export default function SignUp() {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            firstName: data.get("firstName"),
+            lastName: data.get("firstName"),
+            email: data.get("email"),
+            password: data.get("password"),
+            confirmPassword: data.get("confirmPassword")
+        });
+    };
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Formik
+                        initialValues={initialValues}
+                        onSubmit={() => { }}
+                        validationSchema={RegisterSchema}
+                    >
+                        {({ errors, touched, isSubmitting, isValid, dirty }) => (
+                            <Box
+                                onSubmit={handleSubmit}
+                                component="form"
+                                noValidate
+                                sx={{ mt: 1 }}
+                            >
+                                <Field
+                                    as={TextField}
+                                    margin="normal"
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    name="firstName"
+                                    autoComplete="firstName"
+                                />
+                                {errors.firstName && touched.firstName ? (
+                                    <div style={{ color: "red" }}>{errors.firstName}</div>
+                                ) : null}
+                                <Field
+                                    as={TextField}
+                                    margin="normal"
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastName"
+                                    autoComplete="lastName"
+                                />
+                                {errors.lastName && touched.lastName ? (
+                                    <div style={{ color: "red" }}>{errors.lastName}</div>
+                                ) : null}
+                                <Field
+                                    as={TextField}
+                                    margin="normal"
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                />
+                                {errors.email && touched.email ? (
+                                    <div style={{ color: "red" }}>{errors.email}</div>
+                                ) : null}
+                                <Field
+                                    as={TextField}
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                                {errors.password && touched.password ? (
+                                    <div style={{ color: "red" }}>{errors.password}</div>
+                                ) : null}
+                                <Field
+                                    as={TextField}
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="confirmPassword"
+                                    label="Confirm Password"
+                                    type="password"
+                                    id="confirmPassword"
+                                    autoComplete="confirmPassword"
+                                />
+                                {errors.confirmPassword && touched.confirmPassword ? (
+                                    <div style={{ color: "red" }}>{errors.confirmPassword}</div>
+                                ) : null}
+                                <Button
+                                    disabled={!(isValid && dirty)}
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                    action={login}
+                                >
+                                    Sign In
+                                </Button>
+                                <Grid container>
+                                    <Grid item xs>
+                                        <Link to="/">Forgot password?</Link>
+                                    </Grid>
+                                    <Grid item>
+                                        <Link to="/signin">{"Already have an account? Sign In"}</Link>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        )}
+                    </Formik>
+                </Box>
+                <Copyright sx={{ mt: 5 }} />
+            </Container>
+        </ThemeProvider>
+    );
 }
